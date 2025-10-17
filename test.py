d8c34517e0af4e2b4019a6b25c0f94a878afd87d
Богдан <bogdanbalanuk88@gmail.com>
2025-10-17 15:13:38 +0300

    -

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-from streamlit import load_financials, enrich_financials, filter_data, plot_profitability_trends
+from main_body import load_financials, enrich_financials, filter_data, plot_profitability_trends
 
 # 1. Завантаження
 df = load_financials("financials.csv")
 df = enrich_financials(df)
 
 # 2. Фільтр за роками та галуззю
-df_filtered = filter_data(df, years=[2021, 2022], industries=['Energy', 'Tech'])
+df_filtered = filter_data(df, years=[2000, 2023], industries=['Energy', 'Tech'])
 
 # 3. Побудова графіка
-fig = plot_profitability_trends(df_filtered, companies=['ЧДТУ', 'ХНУВС'], metric='NetMargin', title='Net Margin Trend')
+fig = plot_profitability_trends(df_filtered, companies=['Help for every body', 'Free for all'], metric='NetMargin', title='Net Margin Trend')
 fig.show()  # відкриє інтерактивне вікно Plotly
